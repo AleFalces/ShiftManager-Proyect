0ec057755a336f2760632ca4a7772e782302f296
AleFalces <alefalces@gmail.com>
2025-03-09 11:55:54 -0300

    Routing

--- front/Turns-Proyect-Front/src/App.jsx
+++ front/Turns-Proyect-Front/src/App.jsx
@@ -4,15 +4,19 @@ import Home from "./views/Home/Home";
 import LoginForm from "./views/LoginForm/LoginForm";
 import MyTurns from "./views/MyTurns/MyTurns";
 import RegisterForm from "./views/RegisterForm/RegisterForm";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
-  let userHaveAcount = false;
   return (
     <>
       <Navbar />
-      {userHaveAcount === false ? <LoginForm /> : <RegisterForm />}
-      {/* <Home /> */}
-      {/* <MyTurns /> */}
+      <Routes>
+        <Route path={"/"} element={<Home />} />
+        <Route path={"/turns"} element={<MyTurns />} />
+        <Route path={"/register"} element={<RegisterForm />} />
+        <Route path={"/login"} element={<LoginForm />} />
+        <Route path={"/about"} element={<Home />} />
+      </Routes>
     </>
   );
 }
--- front/Turns-Proyect-Front/src/components/NavBar/Navbar.jsx
+++ front/Turns-Proyect-Front/src/components/NavBar/Navbar.jsx
@@ -1,12 +1,19 @@
 import styles from "./NavBar.module.css";
+import { Link } from "react-router-dom";
 
 let Navbar = () => {
+  const userHaveAcount = false;
   return (
     <div className={styles.Navbar}>
-      <li>Home</li>
-      <li>Turns</li>
-      <li>About Us</li>
-      <li>UserPanel</li>
+      <Link to="/">Home</Link>
+      <Link to="/turns">Turns</Link>
+      <Link to="/about">About Us</Link>
+
+      {userHaveAcount === false ? (
+        <Link to="/register">Register</Link>
+      ) : (
+        <Link to="/login">Login</Link>
+      )}
     </div>
   );
 };

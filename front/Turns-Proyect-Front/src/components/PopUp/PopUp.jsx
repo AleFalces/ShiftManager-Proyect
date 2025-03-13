aae615d631ef77b7f133165470fd2cdf90469a52
AleFalces <alefalces@gmail.com>
2025-03-13 12:31:44 -0300

    popUp Changes

--- front/Turns-Proyect-Front/src/components/PopUp/PopUp.jsx
+++ front/Turns-Proyect-Front/src/components/PopUp/PopUp.jsx
@@ -1,28 +1,45 @@
+import { useSelector } from "react-redux";
 import styles from "./PopUp.module.css";
-import { useLocation } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
+import { putReserveTurn } from "../../services/turnsServices";
 
 export const PopUp = ({ turnData, setPopUp }) => {
-  const { day, time } = turnData;
+  const { day, time, turnId } = turnData;
 
   const location = useLocation();
+  const user = useSelector((state) => state.users);
 
   const handldleClose = () => {
     setPopUp(false);
   };
 
-  const handleConfirm = () => {};
+  const handleConfirm = () => {
+    const confirmData = {
+      id: user.users.id,
+      turnId: turnId,
+    };
+
+    putReserveTurn(confirmData);
+  };
 
   return (
     <div className={styles.modal}>
       <p>
-        wants to {location.pathname !== "/myturns" ? "reserve" : "cancel"} rhis
+        wants to {location.pathname !== "/myturns" ? "reserve" : "cancel"} This
         shift?
       </p>
       <p>Day: {day}</p>
       <p> Time: {time}</p>
-      <div>
+      <div className={styles.buttonContainer}>
         <button onClick={handldleClose}> Cancel</button>
-        <button onClick={handleConfirm}> Confirm</button>
+        <br></br>
+        {user.isAuthenticated === false ? (
+          <Link to="/login">
+            <button>you must log in</button>
+          </Link>
+        ) : (
+          <button onClick={handleConfirm}> Confirm</button>
+        )}
       </div>
     </div>
   );

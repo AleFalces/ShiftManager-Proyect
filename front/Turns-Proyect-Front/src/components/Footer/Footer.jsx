dc10301fdc756a7d3c95e0bc3d63c825783703ee
AleFalces <alefalces@gmail.com>
2025-03-14 12:25:55 -0300

    img footer

--- front/Turns-Proyect-Front/src/components/Footer/Footer.jsx
+++ front/Turns-Proyect-Front/src/components/Footer/Footer.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import styles from "./footer.module.css";
+import LogoInsta from "../../assets/instagram.png";
+import LogoFace from "../../assets/facebook.png";
+import LogoWhats from "../../assets/whatsapp.png";
 
 function Footer() {
   return (
     <div className={styles.footer}>
-      <div>whatsap</div>
-      <div>insta</div>
+      <img src={LogoWhats} alt="WhatsApp" />
+      <img src={LogoInsta} alt="Instagram" />
+      <img src={LogoFace} alt="Facebook" />
     </div>
   );
 }

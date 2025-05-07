c7dfd172da43af1cb51d5374ef152e98b5487b1e
AleFalces <alefalces@gmail.com>
2025-05-07 10:44:07 -0300

    app import changes to deploy

--- front/Turns-Proyect-Front/src/App.jsx
+++ front/Turns-Proyect-Front/src/App.jsx
@@ -1,15 +1,14 @@
 import styles from "./App.module.css";
-
 import Navbar from "./components/NavBar/Navbar";
 import Home from "./views/Home/Home";
 import LoginForm from "./views/LoginForm/LoginForm";
 import AvailableTurns from "./views/AvailableTurns/AvailableTurns";
 import RegisterForm from "./views/RegisterForm/RegisterForm";
 import { Route, Routes } from "react-router-dom";
-import Myturns from "./views/MyTurns/MyTurns";
 import UserPanel from "./components/UserPanel/UserPanel";
 import AboutUs from "./components/AboutUs/AboutUs";
 import Footer from "./components/Footer/Footer";
+import MyTurns from "./views/MyTurns/MyTurns";
 
 function App() {
   return (
@@ -19,7 +18,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/turns" element={<AvailableTurns />} />
-          <Route path="/myturns" element={<Myturns />} />
+          <Route path="/myturns" element={<MyTurns />} />
           <Route path="/register" element={<RegisterForm />} />
           <Route path="/login" element={<LoginForm />} />
           <Route path="/userPanel" element={<UserPanel />} />
